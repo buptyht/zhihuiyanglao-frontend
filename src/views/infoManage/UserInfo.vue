d82e5bb7c827fd0d054e12e0e9676676f6029efc
Your Name <you@example.com>
2020-11-26 18:09:04 +0800

    ...

--- src/views/infoManage/UserInfo.vue
+++ src/views/infoManage/UserInfo.vue
@@ -3,32 +3,73 @@
     <div id="left"><img src="./timg.jpg" /></div>
     <div id="right">
       <a-descriptions bordered>
-        <a-descriptions-item label="本人身份证号">100000194910010000</a-descriptions-item>
-        <a-descriptions-item label="本人名字"> 马保国 </a-descriptions-item>
-        <a-descriptions-item label="所在国家"> 中国 </a-descriptions-item>
-        <a-descriptions-item label="所在省份"> 山东 </a-descriptions-item>
-        <a-descriptions-item label="所在城市"> --- </a-descriptions-item>
-        <a-descriptions-item label="所在区县">--- </a-descriptions-item>
-        <a-descriptions-item label="所在详细地址">--- </a-descriptions-item>
-        <a-descriptions-item label="所在经度"> --- </a-descriptions-item>
-        <a-descriptions-item label="所在纬度"> --- </a-descriptions-item>
-        <a-descriptions-item label="本人联系电话">--- </a-descriptions-item>
+        <a-descriptions-item label="本人身份证号">{{ values.identityId }}</a-descriptions-item>
+        <a-descriptions-item label="本人名字"> {{ values.userName }} </a-descriptions-item>
+        <a-descriptions-item label="所在国家"> {{ values.country }} </a-descriptions-item>
+        <a-descriptions-item label="所在省份"> {{ values.province }} </a-descriptions-item>
+        <a-descriptions-item label="所在城市"> {{ values.city }} </a-descriptions-item>
+        <a-descriptions-item label="所在区县">{{ values.region }} </a-descriptions-item>
+        <a-descriptions-item label="所在详细地址">{{ values.address }} </a-descriptions-item>
+        <a-descriptions-item label="所在经度"> {{ center.lng }} </a-descriptions-item>
+        <a-descriptions-item label="所在纬度">{{ center.lat }} </a-descriptions-item>
+        <a-descriptions-item label="本人联系电话">{{ values.phoneNumber }} </a-descriptions-item>
         <a-descriptions-item label="本人有无医保"> ---</a-descriptions-item>
-        <a-descriptions-item label="本人上层组织ID"> ---</a-descriptions-item>
-        <a-descriptions-item label="本人健康状况"> 良好</a-descriptions-item>
-        <a-descriptions-item label="本人平台余额"> ---</a-descriptions-item>
-        <a-descriptions-item label="创建者"> 杨昊天</a-descriptions-item>
-        <a-descriptions-item label="更新者" :span="3"> 杨昊天</a-descriptions-item>
-        <a-descriptions-item label="地图定位"> <baidu-map class="map" center="北京"> </baidu-map></a-descriptions-item>
+        <a-descriptions-item label="本人上层组织ID"> {{ values.orgId }}</a-descriptions-item>
+        <a-descriptions-item label="本人健康状况"> ---</a-descriptions-item>
+        <a-descriptions-item label="本人平台余额"> {{ values.residueMoney }}</a-descriptions-item>
+        <a-descriptions-item label="创建者"> {{ values.createdBy }}</a-descriptions-item>
+        <a-descriptions-item label="更新者" :span="3"> {{ values.updatedBy }}</a-descriptions-item>
+        <a-descriptions-item label="地图定位">  <baidu-map class="map" :center="center" :zoom="zoom" @ready="handler"></baidu-map>
+</a-descriptions-item>
       </a-descriptions>
-      <a-button type="primary" class="editbtn">编辑用户信息</a-button>
+      <a-button type="primary" class="editbtn" @click="$refs.mymodal.setVisibleTrue()">编辑用户信息</a-button>
+      <my-modal ref="mymodal" @ok="setNewValue"></my-modal>
     </div>
   </a-card>
 </template>
 
 <script>
+import MyModal from './modules/MyModal'
 export default {
-  name: 'UserInfo'
+  name: 'UserInfo',
+  components: {
+    MyModal,
+  },
+  data() {
+    return {
+      values: {
+        address: '长安街',
+        city: '北京市',
+        country: '中国',
+        createdBy: 'yht',
+        identityId: '100000195101010000',
+        latitude: '39',
+        longitude: '116',
+        orgId: '1',
+        phoneNumber: '18800000000',
+        province: '北京市',
+        region: '海淀区',
+        residueMoney: '0.5',
+        updatedBy: 'yht',
+        userName: '马保国',
+      },
+      center: { lng: 0, lat: 0 },
+      zoom: 3,
+    }
+  },
+  methods: {
+    setNewValue(values) {
+      this.values = values
+      console.log(this.values)
+    },
+    handler({ BMap, map }) {
+      this.center.lng = 116.404
+      this.center.lat = 39.915
+      this.zoom = 15
+      this.BMap = BMap
+      this.map = map
+    }
+  },
 }
 </script>
 <style scoped>
---
+++ src/views/infoManage/modules/MyModal.vue
@@ -0,0 +1,96 @@
+<template>
+  <a-modal
+    title="操作"
+    style="top: 20px"
+    :width="800"
+    v-model="visible"
+    :confirmLoading="confirmLoading"
+    @ok="handleSubmit"
+  >
+    <a-form :form="form">
+      <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="本人身份证号或组织社会统一信用ID">
+        <a-input placeholder="本人身份证号或组织社会统一信用ID" v-decorator="['identityId']" />
+      </a-form-item>
+      <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="本人名字或组织名字">
+        <a-input placeholder="本人名字或组织名字" v-decorator="['userName']" />
+      </a-form-item>
+      <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="所在国家">
+        <a-input placeholder="所在国家" v-decorator="['country']" />
+      </a-form-item>
+      <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="所在省份">
+        <a-input placeholder="所在省份" v-decorator="['province']" />
+      </a-form-item>
+      <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="所在城市">
+        <a-input placeholder="所在城市" v-decorator="['city']" />
+      </a-form-item>
+      <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="所在区县">
+        <a-input placeholder="所在区县" v-decorator="['region']" />
+      </a-form-item>
+      <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="所在详细地址">
+        <a-input placeholder="所在详细地址" v-decorator="['address']" />
+      </a-form-item>
+      <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="所在经度">
+        <a-input placeholder="所在经度" v-decorator="['longitude']" />
+      </a-form-item>
+      <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="所在纬度">
+        <a-input placeholder="所在纬度" v-decorator="['latitude']" />
+      </a-form-item>
+      <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="本人联系电话或组织负责人电话">
+        <a-input placeholder="本人联系电话或组织负责人电话" v-decorator="['phoneNumber']" />
+      </a-form-item>
+      <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="本人上层组织ID">
+        <a-input placeholder="本人上层组织ID" v-decorator="['orgId']" />
+      </a-form-item>
+      <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="本人平台余额">
+        <a-input placeholder="本人平台余额" v-decorator="['residueMoney']" />
+      </a-form-item>
+      <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="创建者">
+        <a-input placeholder="创建者" v-decorator="['createdBy']" />
+      </a-form-item>
+      <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="更新者">
+        <a-input placeholder="更新者" v-decorator="['updatedBy']" />
+      </a-form-item>
+    </a-form>
+  </a-modal>
+</template>
+<script>
+export default {
+  name: 'MyModal',
+  props: {},
+  components: {},
+  data() {
+    return {
+      visible: false,
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 5 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+      },
+      confirmLoading: false,
+      form: this.$form.createForm(this),
+    }
+  },
+
+  methods: {
+    setVisibleTrue() {
+      this.visible = true
+    },
+    handleSubmit(e) {
+      e.preventDefault()
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values)
+          this.confirmLoading = true
+          this.$emit('ok', values)
+          this.confirmLoading = false
+                this.visible = false
+
+        }
+      })
+    },
+  },
+}
+</script>
